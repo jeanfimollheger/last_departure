2023ad947b67821b6a513e599fe5e5ad7c6b677a
jeanfimollheger <jpmh@gmx.fr>
2026-01-11 23:19:25 +0100

    makemigrations, migrate, and admin models

--- tasks/admin.py
+++ tasks/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import Project, Task
+
 
 # Register your models here.
+# tasks/admin.py
+from django.contrib import admin
+from .models import Project, Task
+
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ("name", "creator", "deadline", "private")
+    prepopulated_fields = {"slug": ("name",)}
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ("description", "project", "area", "done", "deadline")
+    list_filter = ("done", "area", "project")
+    prepopulated_fields = {"slug": ("description",)}
